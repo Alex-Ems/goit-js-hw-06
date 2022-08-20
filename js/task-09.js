12e12cfbb14ad4f77ad8aad1b694bd1652cd3613
Alexandr Hetmanenko <fsdeveloper.alex@gmail.com>
2022-08-20 21:43:53 +0300

    fix 7, 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,14 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const bodyEl = document.querySelector('body');
-const btnEl = document.querySelector('.change-color');
-const colorEl = document.querySelector('.color');
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
 
-btnEl.addEventListener('click', changeBgColorRandom)
-
-function changeBgColorRandom() {
-  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
-  colorEl.textContent = getRandomHexColor();
-  // btnEl.style.backgroundColor = `${getRandomHexColor()}`;
-}
+btnEl.addEventListener("click", () => {
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = `${color}`;
+  colorEl.textContent = color;
+});
